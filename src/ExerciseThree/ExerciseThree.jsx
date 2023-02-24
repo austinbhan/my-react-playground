4b7b08b2f842929dc767112ec7f00f58645da947
austinbhan <austinbhan@outlook.com>
2023-02-24 14:27:48 -0800

    Countries rendered, but browser-side cors fix

--- src/ExerciseThree/ExerciseThree.jsx
+++ src/ExerciseThree/ExerciseThree.jsx
@@ -3,21 +3,34 @@ import styles from '../App.scss';
 
 export default function ExerciseThree() {
 
-  const url = 'https://countryapi.herokuapp.com/countries/';
-  
+  const url = 'https://countryapi.herokuapp.com/countries';
+  const [data, setData] = useState([]);
+
   async function getData() {
     const res = await fetch(url);
-    const realRes = res.json();
-    console.log(realRes);
+    const realRes = await res.json();
+    setData(realRes);
   }
 
-  console.log(getData());
-
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div className={styles.ExerciseThree}>
       <h2>Exercise 3: Import API from Backend</h2>
-      <div className={styles.countries}>
+      <div>
+        {
+          data.map((country, i) => {
+            return <div key = {country + i} className={styles.card}>
+              <h3>{country.country}</h3>
+              <h4>{country.capitol}</h4>
+              <h5>{country.continent}</h5>
+              <h5>{country.spoken_language}</h5>
+              <h5>{country.population}</h5>
+            </div>;
+          })
+        }
       </div>
     </div>
   );
