992a376fb2aa3ccfa2a7cbb382f52c52bd295858
austinbhan <austinbhan@outlook.com>
2023-02-23 16:54:34 -0800

    API access control checks need fixing

--- src/ExerciseThree/ExerciseThree.jsx
+++ src/ExerciseThree/ExerciseThree.jsx
@@ -3,34 +3,21 @@ import styles from '../App.scss';
 
 export default function ExerciseThree() {
 
-  const [countries, setCountries] = useState([]);
-
-  const url = 'http://localhost:7890/countries';
-
+  const url = 'https://countryapi.herokuapp.com/countries/';
+  
   async function getData() {
     const res = await fetch(url);
     const realRes = res.json();
-    setCountries(realRes.countries);
+    console.log(realRes);
   }
 
-  useEffect(() => {
-    getData();
-  }, []);
+  console.log(getData());
 
 
   return (
     <div className={styles.ExerciseThree}>
       <h2>Exercise 3: Import API from Backend</h2>
       <div className={styles.countries}>
-        {
-          countries.map((country, i) => {
-            return <div key={country.id + i}>
-              <div>{country.country}</div>
-              <div>{country.capitol}</div>
-              <div>{country.langue}</div>
-            </div>;
-          })
-        }
       </div>
     </div>
   );
