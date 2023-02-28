5d5c577fe7bd5887362c98d99561a22fbb7c1490
austinbhan <austinbhan@outlook.com>
2023-02-28 14:39:00 -0800

    Install MaterialUI, test button

--- src/ExerciseFour/ExerciseFour.jsx
+++ src/ExerciseFour/ExerciseFour.jsx
@@ -1,9 +1,14 @@
 import styles from '../App.scss';
+import Button from '@mui/material/Button';
 
 export default function ExerciseFour() {
   return (
     <div className={styles.ExerciseFour}>
       <h2>Exercise 4: Use MaterialUI to graphically represent array objects</h2>
+      <Button variant="contained">test button</Button>
     </div>
   );
 }
+
+// Graph: X-axis = Name of country
+// Graph: Y-axis = Population size
