a8f0030120afd5d5fde1f4dfa3adeed7373876d1
austinbhan <austinbhan@outlook.com>
2023-02-28 16:04:09 -0800

    Accordion imported, test display created

--- src/ExerciseFour/ExerciseFour.jsx
+++ src/ExerciseFour/ExerciseFour.jsx
@@ -1,11 +1,26 @@
 import styles from '../App.scss';
-import Button from '@mui/material/Button';
+import { Accordion, Typography } from '@mui/material';
+import { AccordionSummary, AccordionDetails } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function ExerciseFour() {
   return (
     <div className={styles.ExerciseFour}>
-      <h2>Exercise 4: Use MaterialUI to graphically represent array objects</h2>
-      <Button variant="contained">test button</Button>
+      <h2>Exercise 4: Use MaterialUI Accordion 
+        to graphically represent array objects</h2>
+      <Accordion> 
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}>
+          <Typography>
+                Item 1
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            This is the item description
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 }
