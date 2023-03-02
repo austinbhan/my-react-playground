4e4dac6353678eb30fe68f4441bfdd2163f28dce
austinbhan <austinbhan@outlook.com>
2023-03-02 15:37:39 -0800

    Added links to homepage

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import {
-  BrowserRouter as Router,
+  BrowserRouter as Router, Route, Routes,
+  Link
 } from 'react-router-dom';
 import styles from './App.scss';
 import CardList from './Cards/CardList';
@@ -13,11 +14,32 @@ export default function App() {
     <Router>
       <div className={styles.App}>
         <h1 className={styles.header}>React Exercises</h1>
+        <div>
+          <Link to="/exercise-one">
+            <h2>Exercise 1</h2>
+          </Link>
+          <Link to="/exercise-two">
+            <h2>Exercise 2</h2>
+          </Link>
+          <Link to="/exercise-three">
+            <h2>Exercise 3</h2>
+          </Link>
+          <Link to="/exercise-four">
+            <h2>Exercise 4</h2>
+          </Link>
+        </div>
+      
         <div className={styles.exercises}>
-          <CardList cards = {CardData}/>
-          <ExerciseTwo />
-          <ExerciseThree />
-          <ExerciseFour />
+          <Routes>
+            <Route path="/exercise-one" 
+              element={<CardList cards = {CardData}/>}/>
+            <Route path="/exercise-two"
+              element={<ExerciseTwo />} />
+            <Route path="/exercise-three"
+              element={<ExerciseThree />} />
+            <Route path="/exercise-four"
+              element={<ExerciseFour />} />
+          </Routes>
         </div>
       </div>
     </Router>
