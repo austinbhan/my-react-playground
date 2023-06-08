15bb88e340fb902b202e3ac8246d58552a280291
austinbhan <austinbhan@outlook.com>
2023-06-08 15:59:16 -0700

    First Exercise of Exercise 6

--- src/ExerciseSix/ExerciseSix.jsx
+++ src/ExerciseSix/ExerciseSix.jsx
@@ -1,16 +1,33 @@
-export default function ExerciseSix() {
+import { useEffect } from 'react';
 
+export default function ExerciseSix() {
   // -First Exercise-
   // Replace each space within a string with %20.
   // "John Smith" => "John%20Smith"
-  function spaceToString(name) {
-    return name;
+  // if solved, turn problemOne into true
+  
+  function spaceToString(input) {
+    const array = input.split('');
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] != ' ') {
+        newArray.push(array[i]);
+      }
+      else if (array[i] == ' ') {
+        newArray.slice(array);
+        newArray.push('%20');
+      } else return newArray;
+    }
+    console.log(newArray);
   }
+
+  useEffect(() => {
+    spaceToString('Bob and Bill');
+  }, []);
 
   return (
     <div>
       <h2>See console log for exercise results</h2>
-      <p>Exercise 1: {spaceToString('Jim')}</p> 
     </div>
   );
 }
