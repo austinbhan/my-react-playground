6ba90b2a8fd753eac1d20a9ae1bae4dc369747b6
austinbhan <austinbhan@outlook.com>
2023-11-15 13:37:45 -0800

    Instructions in jsx file

--- src/ExerciseTen/ExerciseTen.jsx
+++ src/ExerciseTen/ExerciseTen.jsx
@@ -3,6 +3,9 @@ export default function ExerciseTen() {
   return (
     <div>
       <h1>This is Exercise Ten</h1>
+      <h3>For this exercise, fetch the data for a PostgreSQL 
+        table that was made in terminal </h3>
+      <p></p>
     </div>
   );
 }
